77588712a34ad0141ae7077fe50c33eea74aa858
Quentin Dawans <quentin@quix.io>
2024-08-05 17:40:04 +0200

    Modified using Quix CLI

--- stib-position-source/main.py
+++ stib-position-source/main.py
@@ -29,11 +29,42 @@ class StibVehiculePositionSource(BaseSource):
         while True:
             for line in self.get_vehicule_positions():
                 for vehicle in json.loads(line["vehiclepositions"]):
-                    vehicle["line"] = line["lineid"]
+                    vehicle["lineId"] = line["lineid"]
                     yield self.serialize(line["lineid"], vehicle)
 
             self.sleep(5 * 60)
-        
+
+class StibStopsDetailsSource(BaseSource):
+
+    def __init__(self):
+        self.details = {}
+
+        response = requests.get("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/stop-details-production/exports/json?lang=en&timezone=Europe%2FBerlin")
+        response.raise_for_status()
+        data = response.json()
+        for item in data:
+            name = json.loads(item["name"])
+            coordinates = json.loads(item["gpscoordinates"])
+
+            self.details[item["id"]] = {
+                "coordinates": {
+                    "lat": coordinates["latitude"],
+                    "long": coordinates["longitude"],
+                },
+                "name": name["fr"]
+            }
+
+StopDetails = StibStopsDetailsSource()
+
+def enrich(value):
+    detail = StopDetails.details.get(value["pointId"], {})
+    value.update({
+        "destination_coordinates": detail.get("coordinates", {"lat": 0, "long": 0}),
+        "destination_name": detail.get("name", "unknown"),
+    })
+    return value
+
+
 def main():
     app = Application(
         consumer_group="stib-vehicule-position-source",
@@ -42,11 +73,17 @@ def main():
         auto_offset_reset="earliest"
     )
     
-    topic= app.topic(os.environ["output"])
-    sdf = app.dataframe(source=StibVehiculePositionSource(), topic=topic)
+    source_topic = app.topic(os.environ["source-output"])
+    transform_topic = app.topic(os.environ["transform-output"])
+    
+    sdf = app.dataframe(source=StibVehiculePositionSource(), topic=source_topic)
     # sdf = app.dataframe(topic=topic)
     # sdf = sdf.filter(lambda value: value["line"] == "29")
+
+    sdf = sdf.apply(enrich)
+
     sdf.print()
+    sdf.to_topic(transform_topic)
 
     app.run(sdf)
 
--- stib-waittime-source/main.py
+++ stib-waittime-source/main.py
@@ -27,12 +27,13 @@ class StibWaitTimeSource(BaseSource):
             for item in self.get_waittime():
                 passingtimes = json.loads(item["passingtimes"])
                 for passingtime in passingtimes:
-                    yield self.serialize(
+                    print(passingtime)
+                    yield self.serialize(   
                         key=item["pointid"],
                         value={
-                            "pointid": item["pointid"],
-                            "lineid": item["lineid"],
-                            "destination": passingtime["destination"]["fr"],
+                            "pointId": item["pointid"],
+                            "lineId": item["lineid"],
+                            "destination": passingtime.get("destination", {}).get("fr", ""),
                             "arrival_time": passingtime["expectedArrivalTime"],
                             "message": passingtime.get("message", "")
                         }
@@ -40,6 +41,35 @@ class StibWaitTimeSource(BaseSource):
 
             self.sleep(5 * 60)
 
+
+class StibStopsDetailsSource(BaseSource):
+
+    def __init__(self):
+        self.details = {}
+
+        response = requests.get("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/stop-details-production/exports/json?lang=en&timezone=Europe%2FBerlin")
+        response.raise_for_status()
+        data = response.json()
+        for item in data:
+            name = json.loads(item["name"])
+            coordinates = json.loads(item["gpscoordinates"])
+
+            self.details[item["id"]] = {
+                "coordinates": {
+                    "lat": coordinates["latitude"],
+                    "long": coordinates["longitude"],
+                },
+                "name": name["fr"]
+            }
+
+StopDetails = StibStopsDetailsSource()
+def enrich(value):
+    detail = StopDetails.details.get(value["pointId"], {})
+    value.update({
+        "destination_coordinates": detail.get("coordinates", {"lat": 0, "long": 0}),
+        "destination_name": detail.get("name", "unknown"),
+    })
+    return value
 
 def main():
     app = Application(
@@ -49,11 +79,17 @@ def main():
         auto_offset_reset="earliest"
     )
     
-    topic= app.topic(os.environ["output"])
-    sdf = app.dataframe(source=StibWaitTimeSource(), topic=topic)
+    source_topic = app.topic(os.environ["source-output"])
+    transform_topic = app.topic(os.environ["transform-output"])
+    
+    sdf = app.dataframe(source=StibWaitTimeSource(), topic=source_topic)
     # sdf = app.dataframe(topic=topic)
     # sdf = sdf.filter(lambda value: value["line"] == "29")
+
+    sdf = sdf.apply(enrich)
+
     sdf.print()
+    sdf.to_topic(transform_topic)
 
     app.run(sdf)
 

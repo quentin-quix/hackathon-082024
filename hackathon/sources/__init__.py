b5183e637a028a48b6220522ed1192d7f95e994d
Quentin Dawans <quentin@quix.io>
2024-08-06 16:33:17 +0200

    Modified using Quix CLI

--- hackathon/sources/__init__.py
+++ hackathon/sources/__init__.py
@@ -5,7 +5,7 @@ from quixstreams.sources import IterableBaseSource
 
 class StibVehiculePositionSource(IterableBaseSource):
     def get_vehicule_positions(self):
-        response = requests.get("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/vehicle-position-rt-production/records?limit=100")
+        response = requests.get("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/vehicle-position-rt-production/records?limit=100", headers={"Authorization": "Apikey 4d0b37aa165b9e393ecf9fff5a887fe0e2bf8717a4d1fa8f5b138635"})
         try:
             response.raise_for_status()
         except Exception:
@@ -22,14 +22,14 @@ class StibVehiculePositionSource(IterableBaseSource):
                     vehicle["lineId"] = line["lineid"]
                     yield self.serialize(line["lineid"], vehicle)
 
-            self.sleep(5 * 60)
+            self.sleep(20)
 
 class StibStopsDetailsSource:
 
     def __init__(self):
         self.details = {}
 
-        response = requests.get("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/stop-details-production/exports/json?lang=en&timezone=Europe%2FBerlin")
+        response = requests.get("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/stop-details-production/exports/json?lang=en&timezone=Europe%2FBerlin", headers={"Authorization": "Apikey 4d0b37aa165b9e393ecf9fff5a887fe0e2bf8717a4d1fa8f5b138635"})
         response.raise_for_status()
         data = response.json()
         for item in data:
@@ -46,7 +46,7 @@ class StibStopsDetailsSource:
 
 class StibWaitTimeSource(IterableBaseSource):
     def get_waittime(self):
-        response = requests.get("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/waiting-time-rt-production/exports/json?lang=en&timezone=Europe%2FBerlin")
+        response = requests.get("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/waiting-time-rt-production/exports/json?lang=en&timezone=Europe%2FBerlin", headers={"Authorization": "Apikey 4d0b37aa165b9e393ecf9fff5a887fe0e2bf8717a4d1fa8f5b138635"})
         try:
             response.raise_for_status()
         except Exception:
@@ -72,4 +72,4 @@ class StibWaitTimeSource(IterableBaseSource):
                         }
                     )
 
-            self.sleep(5 * 60)
+            self.sleep(20)
